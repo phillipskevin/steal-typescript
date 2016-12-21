5e9bf9a86406905c092a85140f76e364398fa103
Kevin Phillips <kphillips86@gmail.com>
2016-12-20 23:01:36 -0600

    adding source maps

--- steal-typescript.js
+++ steal-typescript.js
@@ -2,10 +2,22 @@ var ts = require('typescript');
 
 exports.translate = function(load) {
   const result = ts.transpileModule(load.source, {
+    fileName: load.name.slice(0, load.name.indexOf('!')),
     compilerOptions: {
-      module: ts.ModuleKind.CommonJS
+      module: ts.ModuleKind.CommonJS,
+      sourceMap: true
     }
   });
 
-  load.source = result.outputText;
+  var sourceMap = JSON.parse(result.sourceMapText);
+  load.source = result.outputText + '\n//# sourceMappingURL=' + formatSourceMap(sourceMap, load.source);
 };
+
+// convert source map from V3 format to base64
+// source map formatting taken from
+// https://github.com/ilyavf/steal-coffee/blob/master/main.js#L27-L38
+function formatSourceMap(sourceMap, orig){
+  sourceMap.sourcesContent = [orig];
+  sourceMap = JSON.stringify(sourceMap);
+  return 'data:application/json;base64,' + btoa(unescape(encodeURIComponent(sourceMap)));
+}
