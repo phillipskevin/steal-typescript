2c7d37c71cab1397440141356fcdff5abd4fa54d
Kevin Phillips <kphillips86@gmail.com>
2017-01-06 22:05:54 -0600

    adding mocha test for build process

---
+++ test/test-node.js
@@ -0,0 +1,8 @@
+var mocha = require('mocha');
+var assert = require('chai').assert;
+
+describe('steal-typescript - node tests', function() {
+    it('build', function() {
+        assert.ok(true, 'should work');
+    });
+})
