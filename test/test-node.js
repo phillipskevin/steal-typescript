917b8752e990345880c3f14e2f4e223ce3ac6c06
Kevin Phillips <kphillips86@gmail.com>
2017-01-16 22:03:18 -0600

    moving build scripts into their own files

--- test/test-node.js
+++ test/test-node.js
@@ -1,35 +1,23 @@
 var mocha = require('mocha');
 var assert = require('chai').assert;
-var stealTools = require('steal-tools');
+var spawn = require('child_process').spawn;
 
 describe('steal-typescript', function() {
-    it('build - js-main', function() {
-        return stealTools.build({
-            config: __dirname + '/../package.json!npm',
-            main: 'examples/js-main/exclaim.ts!steal-typescript'
-        }, {
-            dest: __dirname + '/../examples/js-main/dist'
-        })
-        .then(function() {
-            assert.ok(true, 'should work');
-        })
-        .catch(function(err) {
-            assert.ok(false, err);
-        });
+    it('build - js-main', function(done) {
+      var build = spawn('node', ['examples/js-main/build.js']);
+
+      build.on('close', function(code) {
+        assert.equal(code, 0, 'should complete successfully');
+        done();
+      });
     });
 
-    it('build - ts-main', function() {
-        return stealTools.build({
-            config: __dirname + '/../package.json!npm',
-            main: 'examples/ts-main/src/exclaim.ts!steal-typescript'
-        }, {
-            dest: __dirname + '/../examples/ts-main/dist'
-        })
-        .then(function() {
-            assert.ok(true, 'should work');
-        })
-        .catch(function(err) {
-            assert.ok(false, err);
-        });
+    it('build - ts-main', function(done) {
+      var build = spawn('node', ['examples/ts-main/build.js']);
+
+      build.on('close', function(code) {
+        assert.equal(code, 0, 'should complete successfully');
+        done();
+      });
     });
 });
